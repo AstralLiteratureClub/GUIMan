45d56600fb8a0081ceee93c72ec42da1d4e07c0a
Antritus <antt.graft@gmail.com>
2024-08-04 12:22:28 +0300

    Add sign handlers

--- src/main/java/bet/astral/signman/SignAction.java
+++ src/main/java/bet/astral/signman/SignAction.java
@@ -1,11 +1,23 @@
 package bet.astral.signman;
 
+import bet.astral.guiman.InventoryGUI;
 import net.kyori.adventure.text.Component;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 
 import java.util.List;
 
+@FunctionalInterface
 public interface SignAction {
-	boolean keepOpen();
+	default SignAction openInventory(InventoryGUI gui) {
+		return (player, lines) -> gui.generateInventory(player);
+	}
+	default SignAction openInventory(Inventory inventory) {
+		return (player, lines) -> player.getScheduler().run(SignGUI.packetHandler.getPlugin(), t->player.openInventory(inventory), null);
+	}
+	default SignAction openSign(SignGUI gui) {
+		return (player, lines) -> gui.open(player);
+	}
+
 	void run(Player player, List<Component> lines);
 }
